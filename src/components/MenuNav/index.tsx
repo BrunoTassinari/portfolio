b1af893bed5091d5fbf645ac76ef54468c949317
Bruno Tassinari <bruno.fagundes80@gmail.com>
2023-04-21 15:04:05 -0300

    add menu

--- src/components/MenuNav/index.tsx
+++ src/components/MenuNav/index.tsx
@@ -1,11 +1,29 @@
 import * as S from './styles';
+import { linkAnchors } from '../../constants/anchors';
 
 interface Props {
   isOpen: boolean;
 }
 
 const MenuNav = ({ isOpen }: Props) => {
-  return <S.MenuNav isOpen={isOpen} />;
+  return (
+    <S.MenuNav isOpen={isOpen}>
+      <S.MenuNavList isOpen={isOpen}>
+        {linkAnchors.map((linkAnchor, index) => {
+          return (
+            <S.MenuLink
+              isOpen={isOpen}
+              key={index}
+              href={linkAnchor.href}
+              title={linkAnchor.label}
+            >
+              {linkAnchor.label}
+            </S.MenuLink>
+          );
+        })}
+      </S.MenuNavList>
+    </S.MenuNav>
+  );
 };
 
 export default MenuNav;
---
+++ src/constants/anchors.ts
@@ -0,0 +1,24 @@
+const linkAnchors = [
+  {
+    href: '#',
+    label: 'Sobre mim',
+  },
+  {
+    href: '#',
+    label: 'Serviços',
+  },
+  {
+    href: '#',
+    label: 'Experiencia',
+  },
+  {
+    href: '#',
+    label: 'Projetos',
+  },
+  {
+    href: '#',
+    label: 'Contato',
+  },
+];
+
+export { linkAnchors };
