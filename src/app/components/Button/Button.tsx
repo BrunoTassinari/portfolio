8ae8e4a4d62e76d71388175e54b255e641697097
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-01-12 23:16:12 -0300

    feat: Button component

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import Mounted from "../Mounted";
+import { getVariantClasses } from "./Button.variants";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  variant: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ children, variant, ...rest }: ButtonProps) => {
+  return (
+    <Mounted>
+      <button
+        type="button"
+        className={`${getVariantClasses(variant)}`}
+        {...rest}
+      >
+        {children}
+      </button>
+    </Mounted>
+  );
+};
+
+export default Button;
