14ed10bdb79cb9a3cfc5df28f67751908523a56e
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-01-14 18:16:35 -0300

    feat: about section

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -9,7 +9,7 @@ type CardProps = {
 const Card = ({ children }: CardProps) => {
   return (
     <Mounted>
-      <div className="container w-[88%] m-auto my-5 flex flex-col items-center justify-center gap-[20px]">
+      <div className="container w-[88%] m-auto my-9 flex flex-col items-center justify-center gap-[20px]">
         {children}
       </div>
     </Mounted>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Card from "@components/Card";
 import Title from "@components/Title";
 import Subtitle from "@components/Subtitle";
 import Paragraph from "@components/Paragraph";
 import Button from "@components/Button";
+
+import skills from "@/app/constants/skills";
+
+import { Github, LinkedinSquare } from "styled-icons/boxicons-logos";
 
 export default function Home() {
   return (
@@ -28,6 +34,45 @@ export default function Home() {
               Download CV
             </a>
           </Button>
+        </div>
+        <div className="w-full flex items-start mt-1">
+          <LinkedinSquare className="h-7 w-7  text-light-tertiary dark:text-dark-tertiary mr-1" />
+          <Github className="h-7 w-7 text-light-tertiary dark:text-dark-tertiary mr-1" />
+        </div>
+      </Card>
+      <Card>
+        <div className="w-full flex flex-col justify-start">
+          <Subtitle>Informações</Subtitle>
+          <Title>Sobre Mim</Title>
+        </div>
+        <Paragraph>
+          Lorem Ipsum é simplesmente uma simulação de texto da indústria
+          tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+          quando um impressor desconhecido pegou uma bandeja de tipos e os
+          embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum é
+          simplesmente uma simulação de texto. Lorem Ipsum é simplesmente uma
+          simulação de texto da indústria tipográfica e de impressos, e vem
+          sendo utilizado desde o século XVI, quando um impressor desconhecido
+          pegou uma bandeja de tipos e os embaralhou para fazer um livro de
+          modelos de tipos. Lorem Ipsum é simplesmente uma simulação de texto.
+        </Paragraph>
+      </Card>
+      <Card>
+        <div className="w-full flex flex-col justify-start">
+          <Subtitle>Conhecimentos</Subtitle>
+          <Title>Tecnologias</Title>
+        </div>
+        <div>
+          <ul>
+            {skills.map((skill) => (
+              <li key={skill.title}>
+                <div className="flex items-center gap-2">
+                  <div>{skill.icon}</div>
+                  <span>{skill.title}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </Card>
     </section>
