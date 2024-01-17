4fd9b425dcbbd6d8579e1f43cd53db37b433aad1
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-01-16 23:00:27 -0300

    feat: intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,17 @@
 "use client";
 
 import Card from "@baseComponents/Card";
-import Title from "@baseComponents/Title";
-import Subtitle from "@baseComponents/Subtitle";
 import Paragraph from "@baseComponents/Paragraph";
-import Button from "@baseComponents/Button";
 import Icon from "@baseComponents/Icon";
-
 import HeaderSection from "@components/HeaderSection/HeaderSection";
 
 import skills from "@/app/constants/skills";
+import Intro from "./components/Intro";
 
 export default function Home() {
   return (
     <section>
-      <Card>
-        <HeaderSection subtitle="Meu nome é" title="Bruno Tassinari" />
-        <Paragraph>
-          Lorem Ipsum é simplesmente uma simulação de texto da indústria
-          tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
-          quando um impressor desconhecido pegou uma bandeja de tipos e os
-          embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum é
-          simplesmente uma simulação de texto.
-        </Paragraph>
-        <section className="w-full flex items-start mt-3">
-          <Button variant="secondary">
-            <span>Entre em contato</span>
-          </Button>
-          <Button variant="primary">
-            <a href="/assets/files/BrunoTassinari.pdf" download>
-              Download CV
-            </a>
-          </Button>
-        </section>
-        <section className="w-full flex items-start mt-1">
-          <Icon
-            icon="linkedin"
-            className="h-7 w-7  text-light-tertiary dark:text-dark-tertiary mr-1"
-          />
-          <Icon
-            icon="github"
-            className="h-7 w-7 text-light-tertiary dark:text-dark-tertiary mr-1"
-          />
-        </section>
-      </Card>
+      <Intro />
       <Card>
         <HeaderSection subtitle="Informações" title="Sobre Mim" />
         <Paragraph>
