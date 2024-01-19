5590256619bcd56544bb98cccfc350dec91c0742
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-01-19 10:47:18 -0300

    refacor: loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Intro from "./components/Intro";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -8,26 +9,28 @@ import Header from "@components/Header";
 import Footer from "./components/Footer";
 import Providers from "./utils/providers";
 
-import Mounted from "@baseComponents/Mounted";
-
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <Providers>
-      <Mounted>
-        <Header />
-        <main
-          className="w-full flex flex-col items-center justify-center 
+      <Header />
+      <main
+        className="w-full flex flex-col items-center justify-center 
   bg-light-primary 
   text-light-text-primary 
   dark:bg-dark-primary 
   dark:text-dark-text-primary"
-        >
-          <Intro />
-          <About />
-          <Skills />
-        </main>
-        <Footer />
-      </Mounted>
+      >
+        <Intro />
+        <About />
+        <Skills />
+      </main>
+      <Footer />
     </Providers>
   );
 }
