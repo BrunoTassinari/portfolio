cce3bd06c33254680efdcc9c05fb69132f8ac408
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-01-28 00:53:44 -0300

    feat: esliint rules

--- src/app/components/ProjectCard/ProjectCard.tsx
+++ src/app/components/ProjectCard/ProjectCard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 import Subtitle from '@components/Subtitle';
+
 import Modal from '@components/Modal';
 import Icon from '@components/Icon';
 
@@ -40,36 +41,38 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
         </div>
       </button>
 
-      <Modal showModal={openDetails}>
-        <div>
-          <Image src={img} alt={project.alt} className="object-cover" />
-        </div>
-        <div className="flex justify-between items-center pt-5 px-4 py-1 border-t-[1px] border-t-light-tertiary dark:border-t-dark-tertiary">
-          <div className="flex items-center">
-            <p className="text-2xl font-bold text-light-text-primary dark:text-dark-text-primary truncate block capitalize mr-3">
-              {project.name}
-            </p>
-            <a href={project.link} target="_blank" rel="noreferrer">
-              <Icon icon="arrowTopRight" />
-            </a>
+      <div className="relative">
+        <Modal showModal={openDetails}>
+          <div>
+            <Image src={img} alt={project.alt} className="object-cover" />
           </div>
-          <button type="button" onClick={toogleOpenDetails}>
-            <Icon icon="xMark" />
-          </button>
-        </div>
-        <div className="px-4 py-1">
-          <p className="text-base text-light-text-primary dark:text-dark-text-primary">
-            {project.description}
-          </p>
-        </div>
-        <div className="px-4 py-1">
-          <p className="text-base text-light-text-primary dark:text-dark-text-primary">
-            {project.stack}
-          </p>
-        </div>
+          <div className="flex justify-between items-center pt-5 px-4 py-1 border-t-[1px] border-t-light-tertiary dark:border-t-dark-tertiary">
+            <div className="flex items-center">
+              <p className="text-2xl font-bold text-light-text-primary dark:text-dark-text-primary truncate block capitalize mr-3">
+                {project.name}
+              </p>
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <Icon icon="arrowTopRight" />
+              </a>
+            </div>
+            <button type="button" onClick={toogleOpenDetails}>
+              <Icon icon="xMark" />
+            </button>
+          </div>
+          <div className="px-4 py-1">
+            <p className="text-base text-light-text-primary dark:text-dark-text-primary">
+              {project.description}
+            </p>
+          </div>
+          <div className="px-4 py-1">
+            <p className="text-base text-light-text-primary dark:text-dark-text-primary">
+              {project.stack}
+            </p>
+          </div>
 
-        <div className="px-4 py-1" />
-      </Modal>
+          <div className="px-4 py-1" />
+        </Modal>
+      </div>
     </div>
   );
 };
