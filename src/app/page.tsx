244d04190aafe7621e91e0d158d5b02f165f4415
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-02-01 22:08:35 -0300

    feat: Theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'next-themes';
 import Home from '@pages/Home';
-import Providers from './utils/providers';
 
 const RootPage = () => {
   const [mounted, setMounted] = useState(false);
@@ -12,9 +12,9 @@ const RootPage = () => {
   if (!mounted) return null;
 
   return (
-    <Providers>
+    <ThemeProvider attribute="class">
       <Home />
-    </Providers>
+    </ThemeProvider>
   );
 };
 
